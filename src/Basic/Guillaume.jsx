c44c62e8a3c6ace0111a572c2c898a08100e1513
Bertrand31 <bertrandjun@gmail.com>
2016-11-21 22:04:23 +0100

    Styled Basic panes

--- src/Basic/Guillaume.jsx
+++ src/Basic/Guillaume.jsx
@@ -6,8 +6,13 @@ const Guillaume = () => (
     <section className="c-pane c-basic c-basic--guillaume">
         <h2 className="c-basic__title">Guillaume</h2>
         <article className="c-basic__content">
-            <p className="c-content">Lorem Ipsum dolor sit amet</p>
-            <p className="c-content">Lorem Ipsum dolor sit amet</p>
+            <p className="c-content">
+                <strong>I'm a Digital Art Director looking for a job in January 2017.</strong><br />
+                &nbsp;I'm a young french-tech lover who currently work at TBWA\Corporate in Paris. My strength lies in websites and motion design.
+            </p>
+            <p className="c-content">
+                If you're interested in my work, feel free to get in touch at <a href="parra.guillaume@gmail.com">parra.guillaume@gmail.com</a>.<br />I'm not doing freelance jobs at the moment.
+            </p>
         </article>
     </section>
 );
--- src/Basic/Interested.jsx
+++ src/Basic/Interested.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import './style.css';
-
 const Interested = () => (
-    <section className="c-pane c-basic c-basic--guillaume">
+    <section className="c-pane c-basic c-basic--interested">
         <h2 className="c-basic__title">Interested?</h2>
         <article className="c-basic__content">
-            <a href="mailto:parra.guillaume@gmail.com" className="c-button">Send me an email</a>
+            <a href="mailto:parra.guillaume@gmail.com" className="c-button c-button--white">Send me an email</a>
             <a href="/public/CV.pdf" className="c-button">Look at my résumé</a>
             <a href="https://dribbble.com/guillaumeparra" className="c-button">My Dribble</a>
         </article>

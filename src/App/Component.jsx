61776d65d3a3e7127f46243df0b93cf64f6fad47
Bertrand31 <bertrandjun@gmail.com>
2016-11-28 13:22:54 +0100

    Improved responsive, added Outre Mer

--- src/App/Component.jsx
+++ src/App/Component.jsx
@@ -11,6 +11,7 @@ import Interested from '../Basic/Interested';
 import Creation from '../Creation/Component';
 import MonitaureContent from '../Creation/monitaure/data.json';
 import EdenParkContent from '../Creation/edenpark/data.json';
+// import OutreMerContent from '../Creation/outremer/data.json';
 import LorealContent from '../Creation/loreal/data.json';
 import StopDjihadContent from '../Creation/stopdjihad/data.json';
 
